27cdf92ff11ecebc5e0f99fdf738cc5e682b6cea
pranto <prantochandrodas@gmail.com>
2022-11-24 02:39:02 +0600

    connect mongo db and call api for get data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,32 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pbafkul.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+        // get category data
+        const productCategoryCollection=client.db('PerfectDealDb').collection('ProductCategoryCollection');
+
+        app.get('/ProductCategory',async(req,res)=>{
+            const query={};
+            const result= await productCategoryCollection.find(query).toArray();
+            res.send(result);
+        });
+
+    }finally{
+
+    }
+}   
+run().catch(error=>console.log(error));
+
+
+
 
 app.get('/', (req, res) => {
     res.send('server is running');
