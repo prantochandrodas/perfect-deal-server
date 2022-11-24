a03813e4d6bca4b01b68639c340286c3c8d055ee
pranto <prantochandrodas@gmail.com>
2022-11-24 19:56:48 +0600

    add jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -14,17 +15,51 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pbafkul.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+const verifyAdmin =async(req,res,next)=>{
+    console.log('inside verify admin ',req.decoded.email);
+    const decodedEmail=req.decoded.email;
+    const query={email:decodedEmail};
+    const user=await usersCollection.findOne(query);
+    if(user?.role !== "admin"){
+        return res.status(403).send({message:'frobidden access'})
+    }
+    next();
+ } 
 async function run(){
     try{
 
+        //JWT
+        app.get('/jwt',async(req,res)=>{
+            const email=req.query.email;
+            const query={
+                email:email
+            }
+            const user=await userCollection.findOne(query);
+            if(user && user.email){
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.send({accessToken:''});
+        });
+
         // get category data
         const productCategoryCollection=client.db('PerfectDealDb').collection('ProductCategoryCollection');
-
-        app.get('/ProductCategory',async(req,res)=>{
+        const userCollection=client.db('PerfectDealDb').collection('usersCollection');
+        app.get('/productCategorys',async(req,res)=>{
             const query={};
             const result= await productCategoryCollection.find(query).toArray();
             res.send(result);
         });
+        
+        // add user
+        app.post('/users',async(req,res)=>{
+            const users=req.body;
+            const result=await userCollection.insertOne(users);
+            res.send(result);
+        });
+
+
 
     }finally{
 
